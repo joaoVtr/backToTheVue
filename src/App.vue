48062edcdb15a6d7a1066a3293932b36bf478fca
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-08-13 22:08:19 -0300

    feat/tests

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader v-show="showHeader" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <div>
+    Two-way data bind
+    <br />
+    <input v-model="name" type="text" />
+    <br />
+    <span v-text="name"> </span>
+  </div>
+  <br />
+
+  <div>
+    <div>On CLick</div>
+    <button @click="test = 'ola Mundo'">Enviar</button>
+    <div @mouseenter="test = ''">
+      {{ test }}
+    </div>
+  </div>
+  <br />
+  <div>
+    <div>Computed Properties</div>
+    <div>{{ user.name }} {{ user.last_name }}</div>
+    <div>{{ fullName }}</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    TheHeader,
+  },
+  data() {
+    return {
+      showHeader: true,
+      name: "joao",
+      test: "",
+      user: {
+        name: "joao",
+        last_name: "vitor",
+      },
+    };
+  },
+  computed: {
+    fullName() {
+      return this.user.name + " " + this.user.last_name + " computed";
+    },
+  },
+};
 </script>
 
 <style>
